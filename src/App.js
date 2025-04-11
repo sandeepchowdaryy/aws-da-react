e91c3e80070c241aa489f33ff88b9508d2a8157e
Tarun Sandeep <tarunsandeep1111@gmail.com>
2025-04-11 10:56:27 +0530

    update code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import display from './display.js';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Sandeep</h1>
+     <h2>AWS DA</h2>
+     <display />
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/display.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const display = () => {
+  return (
+    <div>display</div>
+  )
+}
+
+export default display
